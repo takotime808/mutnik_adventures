b52b38f2451baa6c8bbcf9dcdc2df9d7c1f15362
takotime808 <134562442+takotime808@users.noreply.github.com>
2025-08-14 17:09:26 -0400

    embed PWA icons as data URIs

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'mutnik-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
