316ca06eff00de3dc902473cd9231c57967b7559
tako <cmutnik@gmail.com>
2025-08-16 14:14:45 -0400

    change up image galleries

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ const markers = [
     color: "lightpink",
     url: "galleries/og-mutniks/index.html",
   },
+  {
+    lat: 21.6415,
+    lng: -158.0671,
+    label: "Fam - Dino Feeding",
+    color: "lightpink",
+    url: "images/fam/44e22af4-5c32-4d96-ba3d-48abf198a002.webp",
+  },
   // Zack and Brit
   {
     lat: 36.8508,
@@ -529,8 +536,7 @@ const markers = [
   { lat: 31.5590, lng: 35.4732, label:"Israel", color:"gold", url:"images/tako/DB9A7362.webp" },
   { lat: 29.0384, lng: -81.3789, label:"Orlando", color:"gold", url:"images/tako/DB9A8933.webp" },
   { lat: 43.2136, lng: -123.3413, label:"Oregon", color:"gold", url:"images/tako/oregon.jpeg" },
-  // { lat: 21.5207, lng: -157.8373, label: "Kualoa Ranch", color: "red", url: "images/dukes/44e22af4-5c32-4d96-ba3d-48abf198a002.webp" }, // added to oahu pcc map
-  { lat: 21.6396, lng: -158.6246, label:"Jeff & Beth - Hawaii", color:"red", url:"galleries/jeff-and-beth/oahu_polynesian_cultural_center/index.html" },
+  { lat: 21.6396, lng: -158.6246, label:"Jeff & Beth - Hawaii", color:"red", url:"galleries/jeff-and-beth/oahu/index.html" },
   // { "lat": 29.9511, "lng": -90.0715, "label" : "New Orleans", "color" : "red", "url" : "images/dukes/New Orleans/ab27f212-5ae0-4f8a-9ba6-9c30a8ea1f73.webp" },
   { lat: 29.9511, lng: -90.0715, label:"Jeff & Beth - New Orleans", color:"red", url:"galleries/jeff-and-beth/new_orleans/index.html" },
   // { lat: 43.8429, lng: 10.5027, label: "Pisa", color: "red", url: "images/dukes/IMG_7204.JPG" },
