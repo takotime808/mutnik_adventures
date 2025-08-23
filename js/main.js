f8b2eb03226ed359c3e10d92cb6994544d423b41
tako <cmutnik@gmail.com>
2025-08-23 18:24:15 -0400

    dukes

--- js/main.js
+++ js/main.js
@@ -593,6 +593,7 @@ const markers = [
   { lat: 37.2707, lng: -76.7075, label: "Williamsburg, VA", color: "red", url: "images/dukes/Williamsburg VA.webp" },
   { lat: 34.1477, lng: -118.1443, label: "Pasadena", color: "red", url: "images/dukes/Pasadena_CA.webp" },
   { lat: 43.6532, lng: -79.3832, label: "Toronto 2012", color: "red", url: "galleries/jeff-and-beth/Toronto/index.html" },
+  { lat: 41.1148, lng: -74.1496, label: "Suffern NY", color: "red", url: "images/dukes/Suffern NY.webp" },
   // Captain Kate and Panama Canal were moved to Nenee and Joe pins
   // { lat: 9.11983, lng: -79.738606, label: "Panama Canal", color: "red", url: "images/dukes/IMG_2904_Panama_Canal.png" },
   // { lat: 10.8992, lng: -80.3467, label: "Captain Kate", color: "red", url: "images/dukes/panama_canal/panama_canal_with_captain_kate.jpeg" },
